f4a1b3177a8fe16f161bebd39716d9493b1381ed
JoaoAlvesC <jooao.v@outlook.com>
2022-03-14 21:07:37 -0300

    modulos dos jogos importados, definido menu e estrutura para chamada das funçoes

--- jogos.py
+++ jogos.py
@@ -1,12 +1,15 @@
+import forca
+import adivinhacao
+
 print("*********************************")
 print("Escolha o jogo!")
 print("*********************************")
 
 print("(1) Forca (2) Adivinhação")
 
-jogo = int(input("Qual jogo?"))
+jogo = int(input("Qual jogo? "))
 
 if(jogo == 1):
-    print("Jogo Forca")
+    forca.jogar()
 elif(jogo == 2):
-    print("Jogo adivinhação")
+    adivinhacao.jogar()
